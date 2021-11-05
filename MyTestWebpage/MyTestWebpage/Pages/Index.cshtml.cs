f1a1161f789573bdc7ff4e4d407971177f94914a
ilzemadara.platniecebruzane <ilzemadara.platniecebruzane@va.lv>
2021-11-05 23:16:34 +0200

    Initial project setup and main paga with static data for test list

---
+++ MyTestWebpage/MyTestWebpage/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MyTestWebpage.ViewModels;
+
+namespace MyTestWebpage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var tests = new List<TestListItem>();
+            tests.Add(new TestListItem { Id = 1, Name = "Math" });
+            tests.Add(new TestListItem { Id = 2, Name = "Physics" });
+            tests.Add(new TestListItem { Id = 3, Name = "Biology" });
+            tests.Add(new TestListItem { Id = 4, Name = "Marketing" });
+
+            ViewData["Tests"] = tests;
+        }
+    }
+}
